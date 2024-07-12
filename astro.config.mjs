826976020ed51c0562912551a28f07ed30f23410
byuu <36082241+byuufx@users.noreply.github.com>
2024-07-11 22:35:56 -0300

    updated workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://byuufx.github.io', //https://docs.astro.build/en/guides/deploy/github/
+  base: 'portfolio-dev',
   integrations: [purgecss()]
 });
