83623b5c416bdf73c7928f25540a6b96201b37ae
byuu <36082241+byuufx@users.noreply.github.com>
2024-07-14 15:41:51 -0300

    minor config update (fixed path error)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import purgecss from "astro-purgecss";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://byuufx.github.io', //https://docs.astro.build/en/guides/deploy/github/
-  base: 'portfolio-dev',
+  base: import.meta.env.MODE == 'production' ? '/portfolio-dev/' : '', //FIX for GitHub Pages https://docs.astro.build/en/guides/environment-variables/
   integrations: [purgecss()]
 });
+
